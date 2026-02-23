56eb926df65778d7c938cdd403dc634bccc5f105
ruucm <ruucm.a@gmail.com>
2026-02-23 10:43:57 +0900

    Add sha256 checksum to formula

--- Formula/power-checker.rb
+++ Formula/power-checker.rb
@@ -3,6 +3,7 @@ class PowerChecker < Formula
   homepage "https://github.com/ruucm/power-checker"
   url "https://github.com/ruucm/power-checker/archive/refs/heads/main.tar.gz"
   version "1.0.0"
+  sha256 "4bb0f4ab96110b22411a83969500dacc1c7f88ed00ca42ad6e5eac0917d673bf"
   license "MIT"
 
   def install
